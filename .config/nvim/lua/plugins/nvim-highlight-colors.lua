a978f881ffc229dd6eee9132f94a3e17e7f5d363
yanosea <myanoshi0626@gmail.com>
2023-05-08 19:46:22 +0900

    chore(nvim): replace plugin colorizer

---
+++ .config/nvim/lua/plugins/nvim-highlight-colors.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    -- https://github.com/brenoprata10/nvim-highlight-colors
+    'brenoprata10/nvim-highlight-colors',
+    event = 'BufRead',
+    config = function()
+      require('nvim-highlight-colors').setup({
+        render = 'background',
+        enable_named_colors = true,
+        enable_tailwind = true,
+      })
+    end,
+  },
+}
