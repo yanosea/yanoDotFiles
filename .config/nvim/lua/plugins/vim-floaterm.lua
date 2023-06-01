f6e2fc9eddb0da68838b2811c873336ade0699e6
yanosea <myanoshi0626@gmail.com>
2023-06-01 14:14:03 +0900

    fix(nvim): change keymap to toggle floaterm

--- .config/nvim/lua/plugins/vim-floaterm.lua
+++ .config/nvim/lua/plugins/vim-floaterm.lua
@@ -5,12 +5,11 @@ return {
     lazy = true,
     keys = { '<LEADER>t' },
     cmd = { 'FloatermNew', 'FloatermToggle', 'FloatermSend' },
-    init = function()
-      vim.keymap.set('n', '<LEADER>t', '<CMD>FloatermToggle<CR>')
-    end,
     config = function()
       vim.g.floaterm_title = '  ($1/$2) '
       vim.api.nvim_set_hl(0, 'FloatermBorder', { link = 'Normal' })
+
+      vim.keymap.set('n', '<LEADER>t', '<CMD>FloatermToggle<CR>')
     end,
   },
 }
