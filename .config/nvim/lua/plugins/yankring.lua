2cc00a7b86e7d6a605a00465c5a18b5ee41b58cf
yanosea <myanoshi0626@gmail.com>
2023-06-08 11:27:08 +0900

    fix(nvim): change the timing of loading yankring

--- .config/nvim/lua/plugins/yankring.lua
+++ .config/nvim/lua/plugins/yankring.lua
@@ -3,8 +3,7 @@ return {
     -- https://github.com/vim-scripts/YankRing.vim
     'vim-scripts/YankRing.vim',
     lazy = true,
-    cmd = { 'YRShow' },
-    keys = { '<LEADER>p', '<C-p>', '<C-n>' },
+    event = 'BufReadPost',
     init = function()
       vim.g.yankring_history_dir = os.getenv('XDG_STATE_HOME') .. '/yankring'
       vim.keymap.set('n', '<LEADER>p', '<CMD>YRShow<CR>')
