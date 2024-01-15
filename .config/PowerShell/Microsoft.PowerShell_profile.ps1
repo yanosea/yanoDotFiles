ee103fd3340bdb0c2c4d666f5ed0ca1b15b75bc4
yanosea <myanoshi0626@gmail.com>
2024-01-15 10:33:05 +0900

    feat(powershell): add pwsh profile

---
+++ .config/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,29 @@
+# modules
+## completion
+Import-Module -Name PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Import-Module -Name CompletionPredictor
+# fzf
+Import-Module PSFzf
+Enable-PsFzfAliases
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+# zoxide
+Import-Module ZLocation
+# keymap
+Set-PSReadLineKeyHandler -Key "Ctrl+d" -Function DeleteChar
+Set-PSReadLineKeyHandler -Key "Ctrl+w" -Function BackwardKillWord
+Set-PSReadLineKeyHandler -Key "Ctrl+u" -Function BackwardDeleteLine
+Set-PSReadLineKeyHandler -Key "Ctrl+k" -Function ForwardDeleteLine
+Set-PSReadLineKeyHandler -Key "Ctrl+a" -Function BeginningOfLine
+Set-PSReadLineKeyHandler -Key "Ctrl+e" -Function EndOfLine
+Set-PSReadLineKeyHandler -Key "Ctrl+f" -Function ForwardChar
+Set-PSReadLineKeyHandler -Key "Ctrl+b" -Function BackwardChar
+Set-PSReadLineKeyHandler -Key "Alt+f" -Function NextWord
+Set-PSReadLineKeyHandler -Key "Alt+b" -Function BackwardWord
+Set-PSReadLineKeyHandler -Key "Ctrl+p" -Function PreviousHistory
+Set-PSReadLineKeyHandler -Key "Ctrl+n" -Function NextHistory
+# alias
+Set-Alias ls lsd
+Set-Alias cat bat
+# oh my posh init
+oh-my-posh init pwsh --config $env:POSH_THEMES_PATH/blue-owl.omp.json | Invoke-Expression
