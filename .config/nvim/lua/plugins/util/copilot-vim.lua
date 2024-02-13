c4e2f6d6b497d7e34f150e4065866c18b5dadd73
yanosea <myanoshi0626@gmail.com>
2024-02-14 01:35:53 +0900

    feat(nvim): install github/copilot.vim

---
+++ .config/nvim/lua/plugins/util/copilot-vim.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    -- https://github.com/github/copilot.vim
+    "github/copilot.vim",
+    lazy = false,
+    init = function()
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap("i", "<C-a><TAB>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+      vim.keymap.set("i", "<C-a><C-j>", "<Plug>(copilot-next)")
+      vim.keymap.set("i", "<C-a><C-k>", "<Plug>(copilot-previous)")
+      vim.keymap.set("i", "<C-a><C-d>", "<Plug>(copilot-dismiss)")
+      vim.keymap.set("i", "<C-a><C-s>", "<Plug>(copilot-suggest)")
+   end,
+  },
+}
