a75b785976d429e8bda1037eefff81139f10877d
yanosea <myanoshi0626@gmail.com>
2024-02-15 01:44:19 +0900

    feat(nvim): update mason confing

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -1,5 +1,25 @@
 return {
   {
+    -- https://github.com/williamboman/mason.nvim
+    "williamboman/mason.nvim",
+    dependencies = {
+      -- https://github.com/williamboman/mason-lspconfig.nvim
+      "williamboman/mason-lspconfig.nvim",
+    },
+    init = function()
+      require("mason").setup({
+        ui = {
+          border = "single",
+          icons = {
+            package_installed = ' ',
+            package_pending = '↻ ',
+            package_uninstalled = ' ',
+          },
+        },
+      })
+    end,
+  },
+  {
     -- https://github.com/williamboman/mason-lspconfig.nvim
     "williamboman/mason-lspconfig.nvim",
     dependencies = {
