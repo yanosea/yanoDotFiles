1f05b977ff7ba3f6a97ba3dda018fe5656fcb85c
yanosea <myanoshi0626@gmail.com>
2024-02-22 16:38:08 +0900

    feat(nvim): add test config (ctrl key)

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,5 @@
 -- keymaps
+local key = require('helper.key')
 -- remap space as leader
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
@@ -11,9 +12,9 @@ vim.keymap.set('n', 'k', "gk")
 vim.keymap.set('n', "gj", 'j')
 vim.keymap.set('n', "gk", 'k')
 -- buffer
-vim.keymap.set('n', "<C-k>", "<CMD>bprevious<CR>", { desc = "Move previous buffer" })
-vim.keymap.set('n', "<C-j>", "<CMD>bnext<CR>", { desc = "Move next buffer" })
-vim.keymap.set('n', "<C-t>", "<CMD>enew<CR>", { desc = "New empty buffer" })
-vim.keymap.set('n', "<C-c>", "<CMD>bd<CR>", { desc = "Close buffer" })
+vim.keymap.set('n', key.ctrl_keymap_set(key.ctrl_prefix, "k"), "<CMD>bprevious<CR>", { desc = "Move previous buffer" })
+vim.keymap.set('n', key.ctrl_keymap_set(key.ctrl_prefix, "j"), "<CMD>bnext<CR>", { desc = "Move next buffer" })
+vim.keymap.set('n', key.ctrl_keymap_set(key.ctrl_prefix, "t"), "<CMD>enew<CR>", { desc = "New empty buffer" })
+vim.keymap.set('n', key.ctrl_keymap_set(key.ctrl_prefix, "c"), "<CMD>bd<CR>", { desc = "Close buffer" })
 -- visual
 vim.keymap.set('v', "<C-p>", "\"0p<CR>", { desc = "Paste in a row" })
---
+++ .config/nvim/lua/helper/key.lua
@@ -0,0 +1,17 @@
+local ctrl_prefix
+
+local osName = os.getenv("OS")
+if osName == "Darwin" then
+  ctrl_prefix= "D-"
+else
+  ctrl_prefix= "C-"
+end
+
+local ctrl_keymap_set = function(prefix, key)
+  return "<"..prefix..key..">"
+end
+
+return {
+  ctrl_prefix = ctrl_prefix,
+  ctrl_keymap_set = ctrl_keymap_set
+}
