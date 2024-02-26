357afec5de4f7dacee4b9a25d46958ac7536e3ad
yanosea <myanoshi0626@gmail.com>
2024-02-26 20:21:41 +0900

    fix(nvim): fix plugin config(outline.nvim)

--- .config/nvim/lua/plugins/util/outline-nvim.lua
+++ .config/nvim/lua/plugins/util/outline-nvim.lua
@@ -1,12 +1,12 @@
 return {
   {
-    -- https://github.com/hedyhil/symbols-outline.nvim
-    "hedyhil/symbols-outline.nvim",
+    -- https://github.com/hedyhli/outline.nvim
+    "hedyhli/outline.nvim",
     lazy = true,
     keys = { "<LEADER>o" },
     cmd = { "SymbolsOutline" },
     init = function()
-      require("symbols-outline").setup({})
+      require("outline").setup({})
       vim.keymap.set('n', "<LEADER>o", "<CMD>SymbolsOutline<CR>")
     end,
   },
