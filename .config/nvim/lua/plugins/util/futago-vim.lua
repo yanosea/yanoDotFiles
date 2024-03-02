bea6cffad33e1097aa90cee7ff7427a96feb5aa7
yanosea <myanoshi0626@gmail.com>
2024-03-02 18:22:32 +0900

    fic(nvim) fix plugin config (futago-vim dirs)

--- .config/nvim/lua/plugins/util/futago-vim.lua
+++ .config/nvim/lua/plugins/util/futago-vim.lua
@@ -9,6 +9,9 @@ return {
     lazy = false,
     init = function()
       vim.g['denops#server#deno_args'] = {'-q', '--no-lock', '--unstable-kv', '-A'}
+      vim.g['futago_chat_path'] = os.getenv("GOOGLE_DRIVE") .. "/futago/chat"
+      vim.g['futago_log_file'] = os.getenv("GOOGLE_DRIVE") .. "/futago/log/futago.log"
+      vim.g['futago_history_db'] = os.getenv("GOOGLE_DRIVE") .. "/futago/db/history.db"
       vim.keymap.set('n', "<LEADER>gt", "<CMD>call futago#start_chat()<CR>")
       vim.keymap.set('n', "<LEADER>gf", "<CMD>FutagoHistory<CR>")
     end,
