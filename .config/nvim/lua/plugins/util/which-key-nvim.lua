feff4970fe9e8e6b2bbfd37ecd12bb7fa174bf03
yanosea <myanoshi0626@gmail.com>
2024-03-27 11:50:00 +0900

    ✨feat: add key mappings for which-key plugin in init function

--- .config/nvim/lua/plugins/util/which-key-nvim.lua
+++ .config/nvim/lua/plugins/util/which-key-nvim.lua
@@ -6,6 +6,28 @@ return {
     init = function()
       vim.o.timeout = true
       vim.o.timeoutlen = 300
+      require("which-key").setup()
+      require("which-key").register(
+        {
+          ["<C-l>"] = 'whole lines',
+          ["<C-n>"] = 'keywords in the current file',
+          ["<C-k>"] = 'keywords in dictionary',
+          ["<C-t>"] = 'keywords in thesaurus',
+          ["<C-i>"] = 'keywords in the current and included files',
+          ["<C-]>"] = 'tags',
+          ["<C-f>"] = 'file names',
+          ["<C-d>"] = 'definitions or macros',
+          ["<C-v>"] = 'vim command-line',
+          ["<C-u>"] = 'user defined completion',
+          ["<C-o>"] = 'omni completion',
+          ["<C-s>"] = 'spelling suggestions',
+          ["<C-z>"] = 'stop completion',
+        },
+        {
+          mode = "i",
+          prefix = "<C-x>",
+        }
+      )
     end,
   },
 }
