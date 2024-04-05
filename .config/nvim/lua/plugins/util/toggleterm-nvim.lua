13929af20b3df2ee717e168fb1b22d0c0c3bfc93
yanosea <myanoshi0626@gmail.com>
2024-04-06 04:29:12 +0900

    ✨feat: install neovim plugin (lf.nvim and toggleterm.nvim instead of lf-vim and vim-floaterm)

---
+++ .config/nvim/lua/plugins/util/toggleterm-nvim.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    -- https://github.com/akinsho/toggleterm.nvim
+    "akinsho/toggleterm.nvim",
+    lazy = true,
+    opts = function()
+      require("toggleterm").setup({
+        open_mapping = [[<LEADER>t]],
+        hide_numbers = true,
+        shade_filetypes = {},
+        autochdir = false,
+        start_in_insert = true,
+        insert_mappings = true,
+        terminal_mappings = true,
+        persist_size = true,
+        persist_mode = true,
+        direction = 'horizontal',
+        close_on_exit = true,
+        shell = vim.o.shell,
+        auto_scroll = true,
+      })
+      vim.keymap.set('n', "<LEADER>t", "<CMD>ToggleTerm<CR>", {noremap = true})
+      vim.api.nvim_create_autocmd({ "TermEnter" }, {
+        callback = function()
+          for _, buffers in ipairs(vim.fn.getbufinfo()) do
+            local filetype = vim.api.nvim_buf_get_option(buffers.bufnr, "filetype")
+            if filetype == "toggleterm" then
+              vim.api.nvim_create_autocmd({ "BufWriteCmd", "FileWriteCmd", "FileAppendCmd" }, {
+                buffer = buffers.bufnr,
+                command = "q!",
+              })
+            end
+          end
+        end,
+      })
+    end,
+  },
+}
