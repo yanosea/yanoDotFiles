497364e122680bb2877bb7e2432bb2a8b2c94992
yanosea <myanoshi0626@gmail.com>
2024-04-09 09:37:46 +0900

    🐞fix: fix keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -16,7 +16,6 @@ vim.keymap.set('n', key.ctrl_with_leader_keymap_set(key.ctrl_prefix, "k"), "<CMD
 vim.keymap.set('n', key.ctrl_with_leader_keymap_set(key.ctrl_prefix, "j"), "<CMD>tabnext<CR>", { desc = "Move next tab" })
 vim.keymap.set('n', key.ctrl_with_leader_keymap_set(key.ctrl_prefix, "t"), "<CMD>tabnew<CR>", { desc = "New empty tab" })
 vim.keymap.set('n', key.ctrl_with_leader_keymap_set(key.ctrl_prefix, "c"), "<CMD>tabclose<CR>", { desc = "Close tab" })
-
 -- buffer
 vim.keymap.set('n', key.ctrl_keymap_set(key.ctrl_prefix, "k"), "<CMD>bprevious<CR>", { desc = "Move previous buffer" })
 vim.keymap.set('n', key.ctrl_keymap_set(key.ctrl_prefix, "j"), "<CMD>bnext<CR>", { desc = "Move next buffer" })
