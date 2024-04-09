bdbc7bf452a766ec513320e68408acf8fcc06fee
yanosea <myanoshi0626@gmail.com>
2024-04-09 10:47:52 +0900

    ✨feat: clean unneeded files

--- .config/fish/functions/lf.fish
+++
@@ -1,9 +0,0 @@
-function lf
-    set tempfile (mktemp)
-    command lf -command "map Q \$echo \$PWD >$tempfile; lf -remote \"send \$id quit\"" $argv
-
-    if test -f "$tempfile" -a (cat -- "$tempfile") != (echo -n (pwd))
-        cd -- (cat "$tempfile"); or return
-    end
-    command rm -f -- "$tempfile" ^/dev/null
-end
