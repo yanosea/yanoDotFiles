8a1fb26460fcba4a1681de9458a90eaddba405d4
yanosea <myanoshi0626@gmail.com>
2024-05-07 13:49:40 +0900

    ✨feat: update nvim config (change translate plugin)

---
+++ .config/nvim/lua/plugins/util/denops-translate-vim.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    -- https://github.com/skanehira/denops-translate.vim
+    "skanehira/denops-translate.vim",
+    lazy = false,
+    dependencies = {
+      -- https://github.com/vim-denops/denops.vim
+      "vim-denops/denops.vim"
+    },
+  }
+}
