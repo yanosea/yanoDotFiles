b0f372bc0129dcb2f986f3ec7f33a59abcc94c6e
yanosea <myanoshi0626@gmail.com>
2024-05-13 16:30:05 +0900

    ✨feat: install neovim plugin (copilotchat.nvim)

---
+++ .config/nvim/lua/plugins/util/copilotchat-nvim.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    -- https://github.com/CopilotC-Nvim/CopilotChat.nvim
+    "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = {
+      -- https://github.com/github/copilot.vim
+        "github/copilot.vim",
+      -- https://github.com/nvim-lua/plenary.nvim
+        "nvim-lua/plenary.nvim",
+    },
+    lazy = true,
+    keys = { "<LEADER>ct", },
+    cmd = { "CopilotChatToggle" },
+    opts = {
+      debug = true,
+    },
+
+    init = function()
+      vim.keymap.set('n', "<LEADER>ct", "<CMD>CopilotChatToggle<CR>")
+      vim.keymap.set('n', "<LEADER>cc", "<CMD>CopilotChatStop<CR>")
+    end,
+  },
+}
