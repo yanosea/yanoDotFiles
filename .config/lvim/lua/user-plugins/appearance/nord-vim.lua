c1ffc84403e2f956b94a5593f9cdcd2c01bb6f41
yanosea <myanoshi0626@gmail.com>
2024-05-15 02:18:56 +0900

    🐞fix: update lunarvim config (nord-vim)

--- .config/lvim/lua/user-plugins/appearance/nord-vim.lua
+++ .config/lvim/lua/user-plugins/appearance/nord-vim.lua
@@ -1,5 +1,7 @@
 lvim.plugins = lvim.plugins or {}
 table.insert(lvim.plugins, {
   "arcticicestudio/nord-vim",
+  init = function()
+    lvim.colorscheme = "nord"
+  end
 })
-lvim.colorscheme = "nord"
