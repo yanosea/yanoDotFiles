12a8496c0102c1e12dc2a93f0a61bcfa504e87e9
yanosea <myanoshi0626@gmail.com>
2024-05-15 02:21:24 +0900

    ✨feat: update lunarvim config (copilot.vim)

---
+++ .config/lvim/lua/user-plugins/utils/copilot-vim.lua
@@ -0,0 +1,12 @@
+lvim.plugins = lvim.plugins or {}
+table.insert(lvim.plugins, {
+  "github/copilot.vim",
+  init = function()
+    vim.g.copilot_no_tab_map = true
+    vim.api.nvim_set_keymap("i", "<C-a><TAB>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    vim.keymap.set("i", "<C-a>n", "<Plug>(copilot-next)")
+    vim.keymap.set("i", "<C-a>b", "<Plug>(copilot-previous)")
+    vim.keymap.set("i", "<C-a>d", "<Plug>(copilot-dismiss)")
+    vim.keymap.set("i", "<C-a>s", "<Plug>(copilot-suggest)")
+  end,
+})
