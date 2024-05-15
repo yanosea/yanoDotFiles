6cc85aa9bb3a24120d0dc27faa62354ca1b80d9f
yanosea <myanoshi0626@gmail.com>
2024-05-15 17:23:06 +0900

    ✨feat: update lunarvim config (yankring.vim)

---
+++ .config/lvim/lua/user-plugins/utils/yankring-vim.lua
@@ -0,0 +1,12 @@
+lvim.plugins = lvim.plugins or {}
+table.insert(lvim.plugins, {
+  "vim-scripts/YankRing.vim",
+  cmd = "YRShow",
+  init = function()
+    vim.g.yankring_history_dir = os.getenv("XDG_STATE_HOME") .. "/yankring"
+
+    vim.keymap.set('n', "<LEADER>yh", "<CMD>YRShow<CR>",{ silent = true, desc = "YankRing show"})
+    vim.g.yankring_replace_n_pkey = "<C-y><C-p>"
+    vim.g.yankring_replace_n_nkey = "<C-y><C-n>"
+  end,
+})
