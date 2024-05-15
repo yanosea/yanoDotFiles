2f20b0bb9b127d774c7fac5fe9d37a31aac27709
yanosea <myanoshi0626@gmail.com>
2024-05-15 18:57:40 +0900

    ✨feat: update lunarvim config (winpick.nvim)

---
+++ .config/lvim/lua/user-plugins/utils/winpick-nvim.lua
@@ -0,0 +1,27 @@
+lvim.plugins = lvim.plugins or {}
+table.insert(lvim.plugins, {
+  "gbrlsnchs/winpick.nvim",
+  event = "VimEnter",
+  init = function()
+    require("winpick").setup({
+      border = "single",
+      chars = { "A", "S", "D", "F", "J", "K", "L", ";" }
+    })
+
+    vim.api.nvim_set_keymap("n", "<C-w><C-w>", "<CMD>lua WinPick()<CR>", { noremap = true, silent = true })
+  end,
+})
+
+function WinPick()
+  local win_count = #vim.api.nvim_tabpage_list_wins(0)
+
+  if win_count < 3 then
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w><C-w>", true, false, true), 'n', true)
+  else
+    local success, winid = pcall(require("winpick").select)
+
+    if success and winid then
+      vim.api.nvim_set_current_win(winid)
+    end
+  end
+end
