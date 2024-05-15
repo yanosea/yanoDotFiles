93abd53edc7fb6480b4d78e6c595085178aa8aaf
yanosea <myanoshi0626@gmail.com>
2024-05-15 17:12:41 +0900

    ✨feat: update lunarvim config (fuzzy-motion.vim)

---
+++ .config/lvim/lua/user-plugins/utils/fuzzy-motion-vim.lua
@@ -0,0 +1,14 @@
+lvim.plugins = lvim.plugins or {}
+table.insert(lvim.plugins, {
+  "yuki-yano/fuzzy-motion.vim",
+  dependencies = {
+      "vim-denops/denops.vim",
+      "lambdalisue/kensaku.vim",
+  },
+  event = "VimEnter",
+  init = function()
+    vim.g.fuzzy_motion_matchers = { "fzf", "kensaku" }
+
+    vim.keymap.set('n', "<LEADER><SPACE>", "<CMD>FuzzyMotion<CR>", { silent = true })
+  end,
+})
