f95921eb331eada2e3309f23b0866e73776131db
yanosea <myanoshi0626@gmail.com>
2024-05-15 16:48:32 +0900

    ✨feat: update lunarvim config (copilotchat-nvim.lua)

---
+++ .config/lvim/lua/user-plugins/utils/ai/copilotchat-nvim.lua
@@ -0,0 +1,27 @@
+lvim.plugins = lvim.plugins or {}
+table.insert(lvim.plugins, {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  dependencies = {
+    "github/copilot.vim",
+    {
+      "nvim-lua/plenary.nvim",
+      commit = "f7adfc4b3f4f91aab6caebf42b3682945fbc35be",
+    },
+  },
+  event = "VimEnter",
+  opts = {
+    debug = true,
+  },
+  init = function()
+    vim.keymap.set('n', "<LEADER>Ct", "<CMD>CopilotChat<CR>", { silent = true, desc = "CopilotChat chat" })
+    vim.keymap.set('n', "<LEADER>Cc", "<CMD>CopilotChatStop<CR>", { silent = true, desc = "CopilotChat stop" })
+    vim.api.nvim_set_keymap("n", "<LEADER>Cq", "<cmd>lua CopilotChatBuffer()<cr>", { noremap = true, silent = true, desc = "CopilotChat quick chat" })
+  end,
+})
+
+function CopilotChatBuffer()
+  local input = vim.fn.input("Quick Chat: ")
+  if input ~= "" then
+    require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+  end
+end
