2b2a7516780d25528a937d24382a803ce45c846a
yanosea <myanoshi0626@gmail.com>
2024-05-15 15:04:05 +0900

    ✨feat: update lunarvim config (remove unnecessary keymaps)

--- .config/lvim/lua/config/keymaps.lua
+++ .config/lvim/lua/config/keymaps.lua
@@ -1,8 +1,3 @@
 -- keymaps
 -- hide highlight search
-vim.keymap.set('n', "<ESC>", "<CMD>nohlsearch<CR>", { silent = true, desc = "No Highlight" })
--- line
-vim.keymap.set('n', 'j', "gj")
-vim.keymap.set('n', 'k', "gk")
-vim.keymap.set('n', "gj", 'j')
-vim.keymap.set('n', "gk", 'k')
+vim.keymap.set("n", "<ESC>", "<CMD>nohlsearch<CR>", { silent = true, desc = "No Highlight" })
