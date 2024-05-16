b79304ce5262e07f1a08792f8c550eee88286e8a
yanosea <myanoshi0626@gmail.com>
2024-05-16 22:36:36 +0900

    ✨feat: update lunarvim config (numb.nvim)

---
+++ .config/lvim/lua/user-plugins/utils/numb-nvim.lua
@@ -0,0 +1,14 @@
+lvim.plugins = lvim.plugins or {}
+table.insert(lvim.plugins, {
+  "/nacro90/numb.nvim",
+  event = "VimEnter",
+  init = function()
+    require('numb').setup {
+      show_numbers = true,
+      show_cursorline = true,
+      hide_relativenumbers = true,
+      number_only = false,
+      centered_peeking = true,
+    }
+  end
+})
