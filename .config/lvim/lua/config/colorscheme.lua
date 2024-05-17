0bdacaf1dd1ab9b256be8ec4fc1e98e1bbbee25d
yanosea <myanoshi0626@gmail.com>
2024-05-17 17:13:42 +0900

    ✨feat: update lunarvim config (colorscheme)

--- .config/lvim/lua/config/colorscheme.lua
+++ .config/lvim/lua/config/colorscheme.lua
@@ -3,10 +3,13 @@ lvim.plugins = lvim.plugins or {}
 table.insert(lvim.plugins, {
   -- additional colorschemes
   "arcticicestudio/nord-vim",
+  "neanias/everforest-nvim",
 })
 -- default colorscheme
 lvim.colorscheme = "nord"
 -- change colorscheme keymap
+vim.keymap.set("n", "<C-C>f", "<CMD>colorscheme everforest<CR>",
+  { silent = true, desc = "Toggle colorscheme Everforest" })
 vim.keymap.set("n", "<C-C>n", "<CMD>colorscheme nord<CR>", { silent = true, desc = "Toggle colorscheme Nord" })
 vim.keymap.set("n", "<C-C>l", "<CMD>colorscheme lunar<CR>", { silent = true, desc = "Toggle colorscheme Lunar" })
 vim.keymap.set(
