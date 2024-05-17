62d163f31f74062221cfbf883315a492e24bee96
yanosea <myanoshi0626@gmail.com>
2024-05-17 16:38:08 +0900

    ✨feat: update lunarvim config (lsp-lens.lua)

---
+++ .config/lvim/lua/user-plugins/lsp/lsp-lens-nvim.lua
@@ -0,0 +1,19 @@
+lvim.plugins = lvim.plugins or {}
+table.insert(lvim.plugins, {
+  "VidocqH/lsp-lens.nvim",
+  event = { "BufRead", "BufEnter", },
+  init = function()
+    require("lsp-lens").setup({
+      enable = true,
+      include_declaration = true,
+      sections = {
+        definition = true,
+        references = true,
+        implements = true,
+      },
+      ignore_filetype = {
+        "prisma",
+      },
+    })
+  end
+})
