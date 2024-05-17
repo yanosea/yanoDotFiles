a757c016ab8b528fba455b8a8e9e00af81c42213
yanosea <myanoshi0626@gmail.com>
2024-05-17 14:17:35 +0900

    ✨feat: update lunarvim config (gp.nvim)

--- .config/lvim/lua/lv-settings/overwrite/which-key.lua
+++ .config/lvim/lua/lv-settings/overwrite/which-key.lua
@@ -1,3 +1,12 @@
 -- which-key.nvim
+-- chatgpt
+lvim.builtin.which_key.mappings["a"] = {
+  name = "+chatgpt",
+  t = { "<CMD>GpChatNew tabnew<CR>", "GpChatNew tabnew" },
+  s = { "<CMD>GpChatNew split<<CR>", "GpChatNew split" },
+  v = { "<CMD>GpChatNew vsplit<CR>", "GpChatNew vsplit" },
+  p = { "<CMD>GpChatNew popup<CR>", "GpChatNew popup" },
+  e = { "<CMD>GpChatFinder<CR>", "GpChat finder" },
+}
 -- toggleterm
 lvim.builtin.which_key.mappings["t"] = { "<CMD>ToggleTerm<CR>", "ToggleTerm" }
