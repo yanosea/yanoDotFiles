eee0eb270cc65a7b948099a6674034e8ee408d5b
yanosea <myanoshi0626@gmail.com>
2024-05-18 18:27:06 +0900

    ✨feat: update lunarvim config (winpick.lua)

--- .config/lvim/lua/user-plugins/utils/winpick-nvim.lua
+++ .config/lvim/lua/user-plugins/utils/winpick-nvim.lua
@@ -5,6 +5,26 @@ table.insert(lvim.plugins, {
     require("winpick").setup({
       border = "single",
       chars = { "A", "S", "D", "F", "J", "K", "L", ";" },
+      filter = function(winid)
+        local is_excluded = false
+        local buftype = vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(winid), "buftype")
+        local filetype = vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(winid), "filetype")
+
+        local excluded_buftypes = {
+          "nofile",
+          "prompt"
+        }
+        local excluded_filetypes = {
+          "noice",
+          "notify",
+        }
+
+        if vim.tbl_contains(excluded_buftypes, buftype) or vim.tbl_contains(excluded_filetypes, filetype) then
+          is_excluded = true
+        end
+
+        return not is_excluded
+      end
     })
 
     vim.api.nvim_set_keymap(
