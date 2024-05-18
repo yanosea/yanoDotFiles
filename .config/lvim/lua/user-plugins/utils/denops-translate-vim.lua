5016136a2aa6a21e131ad9839fc6c2e42e351cb5
yanosea <myanoshi0626@gmail.com>
2024-05-18 17:37:17 +0900

    🐞fix: update lunarvim config (denops-translate.vim)

--- .config/lvim/lua/user-plugins/utils/denops-translate-vim.lua
+++ .config/lvim/lua/user-plugins/utils/denops-translate-vim.lua
@@ -2,7 +2,7 @@ lvim.plugins = lvim.plugins or {}
 table.insert(lvim.plugins, {
   "skanehira/denops-translate.vim",
   dependencies = { "vim-denops/denops.vim" },
-  cmd = "Translate",
+  event = { "BufRead", "BufEnter" },
   init = function()
     vim.keymap.set("v", "T", "<CMD>'<,'>Translate<CR>", { noremap = true, silent = true, desc = "Translate" })
   end,
