056397fb09ef61f0bedb2b6abde8cfe6dd17934f
yanosea <myanoshi0626@gmail.com>
2024-05-21 10:08:10 +0900

    ✨feat: update lunarvim config (drop.nvim)

---
+++ .config/lvim/lua/user-plugins/appearance/drop-nvim.lua
@@ -0,0 +1,11 @@
+table.insert(lvim.plugins, {
+  "folke/drop.nvim",
+  event = "VimEnter",
+  init = function()
+    require("drop").setup({
+      theme = "stars",
+      max = 20,
+      interval = 250,
+    })
+  end,
+})
