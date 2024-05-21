026e8cdc30f7b79f2bc32df241670994f8e98c29
yanosea <myanoshi0626@gmail.com>
2024-05-21 10:20:34 +0900

    ✨feat: update lunarvim config (peek.nvim)

---
+++ .config/lvim/lua/user-plugins/utils/peek-nvim.lua
@@ -0,0 +1,20 @@
+table.insert(lvim.plugins, {
+  "toppair/peek.nvim",
+  event = { "VeryLazy" },
+  build = "deno task --quiet build:fast",
+  config = function()
+    require("peek").setup({
+      auto_load = true,
+      close_on_bdelete = true,
+      syntax = true,
+      theme = "dark",
+      update_on_change = true,
+      app = "browser",
+      filetype = { "markdown" },
+      throttle_at = 200000,
+      throttle_time = "auto",
+    })
+    vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+    vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+  end,
+})
