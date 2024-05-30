c09c9873354d2d1149f93bdb11b68128f3f13c9b
yanosea <myanoshi0626@gmail.com>
2024-05-30 15:33:23 +0900

    ✨feat: update hammerspoon config

--- .config/hammerspoon/config/open.lua
+++ .config/hammerspoon/config/open.lua
@@ -1,7 +1,5 @@
 -- activity monitor
 hs.hotkey.bind({ "cmd", "shift" }, "escape", require("helper.util").OpenApp("Activity Monitor"))
--- lanuchpad
-hs.hotkey.bind({}, "ctrl", require("helper.util").OpenApp("Launchpad"))
 -- new finder window
 hs.hotkey.bind({ "ctrl" }, "e", require("helper.util").OpenApp("Finder"))
 -- system settings
